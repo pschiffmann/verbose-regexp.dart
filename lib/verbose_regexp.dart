9716023aed2642f2031a949f98c344254466d571
Philipp Schiffmann <philippschiffmann93@gmail.com>
2018-11-09 20:28:22 +0100

    Run dartfmt --fix

--- lib/verbose_regexp.dart
+++ lib/verbose_regexp.dart
@@ -8,31 +8,31 @@ library verbose_regexp.verbose_regexp;
 /// Whitespace counts as unescaped if it is not preceded by a backslash or
 /// enclosed in a character group.
 /// Unescaped `#` characters starts a line comment, that is ended at `\n`.
-String verbose(String regexp) => regexp.splitMapJoin(
-  _escapeSequence,
-  onMatch: (Match m) => m.group(1) ?? m.group(2),
-  onNonMatch: (String str) => str.replaceAll(_verboseMatcher, '')
-);
+String verbose(String regexp) => regexp.splitMapJoin(_escapeSequence,
+    onMatch: (Match m) => m.group(1) ?? m.group(2),
+    onNonMatch: (String str) => str.replaceAll(_verboseMatcher, ''));
 
 /// Matches backslashes and character groups.
-final RegExp _escapeSequence = new RegExp(
-  r'(?:\\(#|\s))'   // Group 1 matches an escaped whitespace or `#` character.
-                    // Remove the escape character, keep the escaped one.
-  r'|'
-  r'('              // Group 2 filters out stuff that interferes with the other
-                    // replacements. These strings are written back into the
-                    // result unaltered:
-    r'\\[\s\S]'     // - Any escaped character, including backslashes. This way
-                    //   we count the number of backslashes before a whitespace
-                    //   or `#` character.
+final RegExp _escapeSequence = RegExp(
+    // Group 1 matches an escaped whitespace or `#` character.
+    // Remove the escape character, keep the escaped one.
+    r'(?:\\(#|\s))'
     r'|'
-    r'\[('          // - All character groups, because they might include
-      r'\\[\s\S]'   //   whitespace or `#` characters.
-      r'|'
-      r'[^\]]'
-    r')*\]'
-  r')'
-);
+    // Group 2 filters out stuff that interferes with the other
+    // replacements. These strings are written back into the
+    // result unaltered:
+    r'('
+    // - Any escaped character, including backslashes. This way
+    //   we count the number of backslashes before a whitespace
+    //   or `#` character.
+    r'\\[\s\S]'
+    r'|'
+    // - All character groups, because they might include
+    //   whitespace or `#` characters.
+    r'\['
+    r'(?:\\[\s\S]|[^\]])*'
+    r'\]'
+    r')');
 
 /// Matches strings that are treated as verbose and therefore should be ignored.
-final RegExp _verboseMatcher = new RegExp('#[^\n]*\n?|\\s');
+final RegExp _verboseMatcher = RegExp('#[^\n]*\n?|\\s');
